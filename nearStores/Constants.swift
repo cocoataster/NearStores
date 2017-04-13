9f1275d3a4449274d24ccd4e88f9183338dd1568
Eric Sans Alvarez <ericsansalvarez@Erics-MacBook-Pro.local>
2017-04-13 09:16:22 +0200

    final

--- nearStores/Constants.swift
+++ nearStores/Constants.swift
@@ -9,5 +9,9 @@
 import Foundation
 
 let JSON_URL = "https://interview-ios.firebaseio.com/offers.json"
+
+//Test only cupons
 let OnlyCoupons_URL = "https://firebasestorage.googleapis.com/v0/b/json-6bc0d.appspot.com/o/coupons.json?alt=media&token=acdb0b6a-7b50-472d-b9aa-eda37d784e09"
+
+//Test only catalogues
 let OnlyCatalogues_URL = "https://firebasestorage.googleapis.com/v0/b/json-6bc0d.appspot.com/o/catalogues.json?alt=media&token=703b24bb-fa19-4d20-a7fa-d14f953df348"
